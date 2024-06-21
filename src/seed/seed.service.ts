af043b725e6ec541d0940a8b18e6ae8af6670872
Diegoalesco <42728227+Diegoalesco95@users.noreply.github.com>
2024-06-21 09:09:49 -0500

    Add AxiosAdapter for HTTP requests

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,25 +1,25 @@
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
   private readonly url = 'https://pokeapi.co/api/v2/pokemon?limit=650';
 
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({});
 
-    const { data } = await this.axios.get<PokeResponse>(this.url);
+    const data = await this.http.get<PokeResponse>(this.url);
 
     const pokemonToInsert: { name: string; no: number }[] = [];
 
